19d8a4baa4c07c8abf9c1415805bb5c37f6644a6
Kevin <kevainxxx@gmail.com>
2014-12-10 15:36:05 +0100

    Working on CMake for Linux Compile

--- cmake/macros/CheckPlatform.cmake
+++ cmake/macros/CheckPlatform.cmake
@@ -6,5 +6,8 @@
 include("${CMAKE_SOURCE_DIR}/cmake/platform/settings.cmake")
 
 # Include platform settings
-include("${CMAKE_SOURCE_DIR}/cmake/platform/win/settings.cmake")
-set(CMAKE_CXX_FLAGS_RELEASE "/MT")
+if(WIN32)
+    include("${CMAKE_SOURCE_DIR}/cmake/platform/win/settings.cmake")
+elseif(UNIX)
+    include("${CMAKE_SOURCE_DIR}/cmake/platform/unix/settings.cmake")
+endif()
