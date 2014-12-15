b658b6fc53ea3325aa26f5231ad63e141b9da0ad
Kevin <kevainxxx@gmail.com>
2014-12-15 11:23:06 +0100

    Work on FreeBSD CMake port. From https://github.com/Adwelean/Adwelean-Server/commit/699de1fd285cc97705b403840caf23d54f018789 Thanks @Adwelean
    Signed-off-by: Kevin <kevainxxx@gmail.com>

--- cmake/macros/CheckPlatform.cmake
+++ cmake/macros/CheckPlatform.cmake
@@ -5,9 +5,17 @@
 # Include global settings
 include("${CMAKE_SOURCE_DIR}/cmake/platform/settings.cmake")
 
+if (${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+    set(FREEBSD TRUE)
+endif()
+
 # Include platform settings
-if(WIN32)
+if (WIN32)
     include("${CMAKE_SOURCE_DIR}/cmake/platform/win/settings.cmake")
-elseif(UNIX)
-    include("${CMAKE_SOURCE_DIR}/cmake/platform/unix/settings.cmake")
+elseif (UNIX)
+    if (FREEBSD)
+        include("${CMAKE_SOURCE_DIR}/cmake/platform/freebsd/settings.cmake")
+    else()
+        include("${CMAKE_SOURCE_DIR}/cmake/platform/unix/settings.cmake")
+    endif()
 endif()
