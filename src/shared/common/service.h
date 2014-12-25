93f143663304614e6d545de896cb4c74097097c8
Adwelean <quentin.joseph@outlook.com>
2014-12-25 12:34:02 +0100

    Revert "Disable _IMPROVED_PACKET_ENCRYPTION_"
    This reverts commit 2e1f33166225ec566e1a2ebfa8c16814da419e81.

--- src/shared/common/service.h
+++ src/shared/common/service.h
@@ -1,7 +1,7 @@
 #ifndef __INC_SERVICE_H__
 #define __INC_SERVICE_H__
 
-//#define _IMPROVED_PACKET_ENCRYPTION_ // ЖРЕ¶ ѕПИЈИ­ °іј±
+#define _IMPROVED_PACKET_ENCRYPTION_ // ЖРЕ¶ ѕПИЈИ­ °іј±
 //#define __AUCTION__
 #define __PET_SYSTEM__
 #define __UDP_BLOCK__
